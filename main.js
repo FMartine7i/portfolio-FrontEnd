c6a361b4d9d2a90f900480ebc7c83fff78536ce7
Ruined-King <frmmartinetti98@gmail.com>
2022-09-05 13:27:11 -0300

    archivos actualizados

--- main.js
+++ main.js
@@ -47,12 +47,76 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrollHeader) 
 
 /* Scroll Reveal animation */
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60',
-    duration: '2500',
-})
+window.sr = ScrollReveal({reset: true});
 
-sr.reveal(`.home__info`, {delay: 1500})
-sr.reveal(`.home__description`, {delay: 1200})
-sr.reveal(`.home__foter`, {delay: 1600})
+    sr.reveal('.text.second-text',{
+        duration: 3000,
+        delay: 1200,
+        origin: 'top',
+        distance: -60,
+    })
+
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60',
+//     duration: '2500',
+// })
+
+// sr.reveal(`.home__title`, {delay: 1500})
+// sr.reveal(`h2`, {delay: 1500})
+// sr.reveal('.home__description', {delay: 1200})
+// sr.reveal(`.home__footer`, {delay: 1600})
+
+/*      TYPING ANIMATION     */
+const text = document.querySelector(".first-text");
+
+const textLoad = () => {
+    setTimeout(() =>{
+        text.textContent = "Hola, soy";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "Hello, I'm";
+    }, 4000, 0);
+}
+textLoad();
+setInterval(textLoad, 8000);
+
+// SOCIAL BUTTONS ANIMATION
+const buttons = document.querySelectorAll('.social-buttons button');
+
+                buttons.forEach(button => {
+
+                    button.addEventListener('mousemove', e => {
+                        const x = e.layerX - 60;
+                        const y = e.layerY - 60;
+                        const i = e.target.querySelector('i');
+                        const bg = e.target.querySelector('.bg');
+                        i.style.transform = `translate(${x / 4}px, ${y / 4}px)`;
+                        bg.style.transform = `translate(${x / 8}px, ${y / 8}px)`;
+                    });
+
+                    button.addEventListener('mouseenter', e => {
+                        const i = e.target.querySelector('i');
+                        const bg = e.target.querySelector('.bg');
+                        i.style.transition = 'all .15s ease';
+                        bg.style.transition = 'all .15s ease';
+                        setTimeout(() => {
+                            i.style.transition = '';
+                            bg.style.transition = '';    
+                        }, 150);
+                    });
+
+                    button.addEventListener('mouseleave', e => {
+                        const i = e.target.querySelector('i');
+                        const bg = e.target.querySelector('.bg');
+                        i.style.transition = 'all .25s ease';
+                        bg.style.transition = 'all .25s ease';
+                        i.style.transform = `translate(${0}px, ${0}px)`;
+                        bg.style.transform = `translate(${0}px, ${0}px)`;
+                        setTimeout(() => {
+                            i.style.transition = '';
+                            bg.style.transition = '';    
+                        }, 250);
+                    });
+
+                });
